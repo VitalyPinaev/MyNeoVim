b23c42377a7093327f6a6e7fb6633b62d8d81fb3
Vitaly Pinaev <pinnaev28@gmail.com>
2024-10-13 21:01:09 +0300

    delete files

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -1,15 +1,21 @@
+------------- 
+-- Options --
+-------------
 vim.opt.nu = true -- Enable line numbers
 vim.opt.tabstop = 4 -- Set the width of a tab to 4 spaces
 vim.o.syntax = true -- Enable syntax highlighting
 vim.opt.numberwidth = 1 -- Set the width of the line number column
-vim.cmd.colorscheme("catppuccin-frappe") -- Apply the specified colorscheme
 vim.opt.shiftwidth = 4 -- Set the number of spaces for indentation
 vim.opt.softtabstop = 4 -- Set the number of spaces per tab in insert mode
 vim.opt.smartindent = true -- Enable intelligent indentation
 vim.opt.scrolloff = 5 -- Keep 5 lines of context when scrolling
-vim.cmd.highlight("Comment guifg='Gray'") -- Change the color of comments to SeaGreen
 vim.opt.autoindent = true -- Preserve indentation from the previous line
-vim.opt.ignorecase = true -- Отключить учет регистров 
-vim.opt.cursorline = true -- Подсветить строку на которой находится курсор
-vim.cmd("setlocal showtabline=0") -- Statusline none
+vim.opt.ignorecase = true -- Disable case sensitivity
+vim.opt.cursorline = true -- Highlight the line where the cursor is located
 
+---------
+-- Cmd --
+---------
+vim.cmd.highlight("Comment guifg='Gray'") -- Change the color of comments to SeaGreen
+vim.cmd("setlocal showtabline=0") -- Statusline none
+vim.cmd.colorscheme("catppuccin-frappe") -- Apply the specified colorscheme
--- lua/config/keyBindings.lua
+++ lua/config/keyBindings.lua
@@ -1,15 +1,22 @@
--- Function for simple make map
+-------------------------------------
+-- Function for simple make HotKey -- 
+-------------------------------------
 local function map(mode, lsh, rsh, opts)
   opts = { noremap = true, silent = true }
   vim.keymap.set(mode, lsh, rsh, opts)
 end
 
+
+-------------
+-- HotKeys --
+-------------
+
 -- Save file
-map("i", "<C-s>", "<Esc>:w<CR>")
-map("n", "<C-s>", ":w<CR>")
+map('i', '<C-s>', '<Esc>:w<CR>')
+map('n', '<C-s>', ':w<CR>')
 
 -- Copy in system buffer
-map('v', '<leader>c', '"+y')
+map('v', '<leader>c', '+y')
 
 -- Save and quit
 map('i', '<C-q>', '<Esc>:wq<CR>')
@@ -20,24 +27,24 @@ map('i', '<M-l>', '<Esc>V"+y')
 map('n', '<M-l>', 'V"+y')
 
 -- Move line up 
-map('n', '<C-Up>', 'ddkkp==')
 map('i', '<C-Up>', '<Esc>ddkkp==')
+map('n', '<C-Up>', 'ddkkp==')
 
 -- Move line down 
 map('i', '<C-Down>', '<Esc>0v$yddp==')
 map('n', '<C-Down>', '0v$yddp==')
 
 -- BufferNext 
-map('n', '<C-u>', ':BufferNext<CR>' )
 map('i', '<C-u>', '<Esc> :BufferNext<CR>' )
+map('n', '<C-u>', ':BufferNext<CR>' )
 
 -- BufferClose
 map('n', '<C-m>', ':w<CR> :BufferClose<CR>')
 
 require'nvim-tree'.setup {}
 -- Open or close NvimTree 
-map('n', '<C-n>', ':lua require("nvim-tree.api").tree.toggle()<CR>')
 map('i', '<C-n>', '<Esc> :lua require("nvim-tree.api").tree.toggle()<CR>')
+map('n', '<C-n>', ':lua require("nvim-tree.api").tree.toggle()<CR>')
 
 -- copy line and paste on next line
 map('i', '<C-d>', '<esc>0v$hyo<esc>0p<esc>')
@@ -47,17 +54,14 @@ map('n', '<C-d>', '0v$hyo<esc>0p<esc>')
 map('i', '<M-w>', '<Esc>daw')
 map('n', '<M-w>', 'daw')
 
--- Выделить всё внутри круглых скобок
+-- Select everything inside parentheses
 map('i', '<M-(>', '<Esc>vib')
 map('n', '<M-(>', 'vib')
 
--- Выделить всё внутри фигурных скобок
+-- Select everything inside the curly braces
 map('i', '<M-{>', '<Esc>viB')
 map('n', '<M-{>', 'viB')
 
-local builtin = require('telescope.builtin')
-map('i', '<C-p>', "<ESC> :w<CR> :Telescope find_files<CR>")
-map('n', '<C-p>', ":w<CR> :Telescope find_files<CR>")
-map('n', '<leader>fg', builtin.live_grep)
-map('n', '<leader>fb', builtin.buffers)
-map('n', '<leader>fh', builtin.help_tags)
+-- Open telescope
+map('i', '<C-p>', '<ESC> :w<CR> :Telescope find_files<CR>')
+map('n', '<C-p>', ':w<CR> :Telescope find_files<CR>')
